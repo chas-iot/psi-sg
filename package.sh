36e146d56fdec1004d476b5e0757a42371a73da4
chas-iot <chas-iot@runegate.org>
2020-07-02 00:07:47 +0800

    fix packaging process

--- package.sh
+++ package.sh
@@ -5,7 +5,7 @@ rm -rf node_modules
 # If you have npm production dependencies, uncomment the following line
 npm install --production
 
-shasum --algorithm 256 manifest.json package.json *.js LICENSE README.md > SHA256SUMS
+shasum --algorithm 256 manifest.json package.json *.js  src/*.js LICENSE README.md > SHA256SUMS
 find css js views -type f -exec shasum --algorithm 256 {} \; >> SHA256SUMS
 
 # If you have npm production dependencies, uncomment the following line
